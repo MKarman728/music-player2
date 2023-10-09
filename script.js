8620a17558c04a65f9e18c6ed2656660b3a33d13
Mark Karman <mkarman08@gmail.com>
2023-10-09 21:53:14 +0900

    next, prev song, and progress

--- script.js
+++ script.js
@@ -42,10 +42,43 @@ function pauseSong(){
     audio.pause();
 }
 
+// Previous Song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Update progress bar
+function updateProgress(e){
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width)* duration;
+}
 
 // Event listeners
 playBtn.addEventListener('click', ()=>{
-    console.log(musicContainer);
     const isPlaying = musicContainer.classList.contains('play');
     if(isPlaying){
         pauseSong();
@@ -53,3 +86,16 @@ playBtn.addEventListener('click', ()=>{
         playSong();
     }
 })
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
